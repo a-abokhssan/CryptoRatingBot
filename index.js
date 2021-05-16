91a4f9def018146513ec0ade60a130bf5242558f
Amir Abokhssan <a.abokhssan@gmail.com>
2021-05-16 15:50:07 +0300

    added get response and format answer message

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Telegraf } = require("telegraf");
+const axios = require("axios");
 
 const TELEGRAM_BOT_TOKEN =
   process.env.TELEGRAM_BOT_TOKEN ||
@@ -9,4 +10,33 @@ const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
 bot.start((ctx) => {
   return ctx.reply("Welcome to CryptoRating Bot!");
 });
+
+bot.command("rates", async (ctx) => {
+  let qs = `?start=1&limit=10&convert=USD`;
+
+  try {
+    let res = await axios.get(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest" +
+        qs,
+      {
+        headers: {
+          "X-CMC_PRO_API_KEY": "03cd5146-2aff-4715-88ca-0e2acbffe9b7",
+        },
+      }
+    );
+    return ctx.reply(
+      `${res.data.data
+        .map(
+          (it) =>
+            `${it.symbol} | 🇺🇸 ${it.quote["USD"].price.toFixed(2)} | ${it.quote[
+              "USD"
+            ]["percent_change_24h"].toFixed(2)}%`
+        )
+        .join("\n")}`
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 bot.startPolling();
