200d29280116ca4ac583e0905918a52101aabbff
Amir Abokhssan <a.abokhssan@gmail.com>
2021-05-16 10:05:56 +0300

    init project && add welcome message

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require("telegraf");
+
+const TELEGRAM_BOT_TOKEN =
+  process.env.TELEGRAM_BOT_TOKEN ||
+  "1690572286:AAG60I8zQQcmaQNkE_K1WtMajIDH3Il3zxQ";
+
+const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+
+bot.start((ctx) => {
+  return ctx.reply("Welcome to CryptoRating Bot! ");
+});
+bot.startPolling();
